b51dd77c6898790f4d560a155741387c63cfcc9e
mohamedezzatseyam <drezzat3@gmail.com>
2020-11-22 12:02:05 +0300

    newcommit

---
+++ app/Gallery.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Gallery extends Model
+{
+    protected  $fillable = ['user_id', 'image_url'];
+
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
